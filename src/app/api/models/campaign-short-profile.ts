9411ea11de14c3ed1c114121317e5446d1ef0fbc
hadware <hadwarez@gmail.com>
2019-11-04 14:17:54 +0100

    Forgot to add some new service files

---
+++ src/app/api/models/campaign-short-profile.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+export interface CampaignShortProfile  {
+  name: string;
+  slug: string;
+}
---
+++ src/app/api/services/downloads.service.ts
@@ -0,0 +1,321 @@
+/* tslint:disable */
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { BaseService } from '../base-service';
+import { ApiConfiguration } from '../api-configuration';
+import { StrictHttpResponse } from '../strict-http-response';
+import { RequestBuilder } from '../request-builder';
+import { Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+
+import { TaskTextGridList } from '../models/task-text-grid-list';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DownloadsService extends BaseService {
+  constructor(
+    config: ApiConfiguration,
+    http: HttpClient
+  ) {
+    super(config, http);
+  }
+
+  /**
+   * Path part for operation downloadsTaskTaskIdStarterGet
+   */
+  static readonly DownloadsTaskTaskIdStarterGetPath = '/downloads/task/{task_id}/starter';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `downloadsTaskTaskIdStarterGet()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsTaskTaskIdStarterGet$Response(params: {
+    taskId: string;
+
+  }): Observable<StrictHttpResponse<void>> {
+
+    const rb = new RequestBuilder(this.rootUrl, DownloadsService.DownloadsTaskTaskIdStarterGetPath, 'get');
+    if (params) {
+
+      rb.path('task_id', params.taskId);
+
+    }
+    return this.http.request(rb.build({
+      responseType: 'text',
+      accept: '*/*'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return (r as HttpResponse<any>).clone({ body: undefined }) as StrictHttpResponse<void>;
+      })
+    );
+  }
+
+  /**
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `downloadsTaskTaskIdStarterGet$Response()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsTaskTaskIdStarterGet(params: {
+    taskId: string;
+
+  }): Observable<void> {
+
+    return this.downloadsTaskTaskIdStarterGet$Response(params).pipe(
+      map((r: StrictHttpResponse<void>) => r.body as void)
+    );
+  }
+
+  /**
+   * Path part for operation downloadsTaskTaskIdCurrentTextgridGet
+   */
+  static readonly DownloadsTaskTaskIdCurrentTextgridGetPath = '/downloads/task/{task_id}/current_textgrid';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `downloadsTaskTaskIdCurrentTextgridGet()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsTaskTaskIdCurrentTextgridGet$Response(params: {
+    taskId: string;
+
+  }): Observable<StrictHttpResponse<void>> {
+
+    const rb = new RequestBuilder(this.rootUrl, DownloadsService.DownloadsTaskTaskIdCurrentTextgridGetPath, 'get');
+    if (params) {
+
+      rb.path('task_id', params.taskId);
+
+    }
+    return this.http.request(rb.build({
+      responseType: 'text',
+      accept: '*/*'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return (r as HttpResponse<any>).clone({ body: undefined }) as StrictHttpResponse<void>;
+      })
+    );
+  }
+
+  /**
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `downloadsTaskTaskIdCurrentTextgridGet$Response()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsTaskTaskIdCurrentTextgridGet(params: {
+    taskId: string;
+
+  }): Observable<void> {
+
+    return this.downloadsTaskTaskIdCurrentTextgridGet$Response(params).pipe(
+      map((r: StrictHttpResponse<void>) => r.body as void)
+    );
+  }
+
+  /**
+   * Path part for operation downloadsTaskTaskIdConflictLogGet
+   */
+  static readonly DownloadsTaskTaskIdConflictLogGetPath = '/downloads/task/{task_id}/conflict_log';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `downloadsTaskTaskIdConflictLogGet()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsTaskTaskIdConflictLogGet$Response(params: {
+    taskId: string;
+
+  }): Observable<StrictHttpResponse<void>> {
+
+    const rb = new RequestBuilder(this.rootUrl, DownloadsService.DownloadsTaskTaskIdConflictLogGetPath, 'get');
+    if (params) {
+
+      rb.path('task_id', params.taskId);
+
+    }
+    return this.http.request(rb.build({
+      responseType: 'text',
+      accept: '*/*'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return (r as HttpResponse<any>).clone({ body: undefined }) as StrictHttpResponse<void>;
+      })
+    );
+  }
+
+  /**
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `downloadsTaskTaskIdConflictLogGet$Response()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsTaskTaskIdConflictLogGet(params: {
+    taskId: string;
+
+  }): Observable<void> {
+
+    return this.downloadsTaskTaskIdConflictLogGet$Response(params).pipe(
+      map((r: StrictHttpResponse<void>) => r.body as void)
+    );
+  }
+
+  /**
+   * Path part for operation downloadsTaskTaskIdTextgridsGet
+   */
+  static readonly DownloadsTaskTaskIdTextgridsGetPath = '/downloads/task/{task_id}/textgrids';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `downloadsTaskTaskIdTextgridsGet()` instead.
+   *
+   * This method sends `application/json` and handles response body of type `application/json`
+   */
+  downloadsTaskTaskIdTextgridsGet$Response(params: {
+    taskId: string;
+
+    body: TaskTextGridList
+  }): Observable<StrictHttpResponse<void>> {
+
+    const rb = new RequestBuilder(this.rootUrl, DownloadsService.DownloadsTaskTaskIdTextgridsGetPath, 'get');
+    if (params) {
+
+      rb.path('task_id', params.taskId);
+
+      rb.body(params.body, 'application/json');
+    }
+    return this.http.request(rb.build({
+      responseType: 'text',
+      accept: '*/*'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return (r as HttpResponse<any>).clone({ body: undefined }) as StrictHttpResponse<void>;
+      })
+    );
+  }
+
+  /**
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `downloadsTaskTaskIdTextgridsGet$Response()` instead.
+   *
+   * This method sends `application/json` and handles response body of type `application/json`
+   */
+  downloadsTaskTaskIdTextgridsGet(params: {
+    taskId: string;
+
+    body: TaskTextGridList
+  }): Observable<void> {
+
+    return this.downloadsTaskTaskIdTextgridsGet$Response(params).pipe(
+      map((r: StrictHttpResponse<void>) => r.body as void)
+    );
+  }
+
+  /**
+   * Path part for operation downloadsTextgridTextgridIdGet
+   */
+  static readonly DownloadsTextgridTextgridIdGetPath = '/downloads/textgrid/{textgrid_id}';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `downloadsTextgridTextgridIdGet()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsTextgridTextgridIdGet$Response(params: {
+    textgridId: string;
+
+  }): Observable<StrictHttpResponse<void>> {
+
+    const rb = new RequestBuilder(this.rootUrl, DownloadsService.DownloadsTextgridTextgridIdGetPath, 'get');
+    if (params) {
+
+      rb.path('textgrid_id', params.textgridId);
+
+    }
+    return this.http.request(rb.build({
+      responseType: 'text',
+      accept: '*/*'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return (r as HttpResponse<any>).clone({ body: undefined }) as StrictHttpResponse<void>;
+      })
+    );
+  }
+
+  /**
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `downloadsTextgridTextgridIdGet$Response()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsTextgridTextgridIdGet(params: {
+    textgridId: string;
+
+  }): Observable<void> {
+
+    return this.downloadsTextgridTextgridIdGet$Response(params).pipe(
+      map((r: StrictHttpResponse<void>) => r.body as void)
+    );
+  }
+
+  /**
+   * Path part for operation downloadsCampaignCampaignSlugGet
+   */
+  static readonly DownloadsCampaignCampaignSlugGetPath = '/downloads/campaign/{campaign_slug}';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `downloadsCampaignCampaignSlugGet()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsCampaignCampaignSlugGet$Response(params: {
+    campaignSlug: string;
+
+  }): Observable<StrictHttpResponse<void>> {
+
+    const rb = new RequestBuilder(this.rootUrl, DownloadsService.DownloadsCampaignCampaignSlugGetPath, 'get');
+    if (params) {
+
+      rb.path('campaign_slug', params.campaignSlug);
+
+    }
+    return this.http.request(rb.build({
+      responseType: 'text',
+      accept: '*/*'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return (r as HttpResponse<any>).clone({ body: undefined }) as StrictHttpResponse<void>;
+      })
+    );
+  }
+
+  /**
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `downloadsCampaignCampaignSlugGet$Response()` instead.
+   *
+   * This method doesn't expect any response body
+   */
+  downloadsCampaignCampaignSlugGet(params: {
+    campaignSlug: string;
+
+  }): Observable<void> {
+
+    return this.downloadsCampaignCampaignSlugGet$Response(params).pipe(
+      map((r: StrictHttpResponse<void>) => r.body as void)
+    );
+  }
+
+}
