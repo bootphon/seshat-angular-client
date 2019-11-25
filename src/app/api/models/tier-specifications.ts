65268eca41dea662a8bfffdbc8183aa33bedf822
hadware <hadwarez@gmail.com>
2019-11-25 17:15:31 +0100

    Fixed the checking scheme display.

--- src/app/api/models/tier-specifications.ts
+++ src/app/api/models/tier-specifications.ts
@@ -2,7 +2,7 @@
 export interface TierSpecifications  {
   allow_empty?: boolean;
   categories?: Array<string>;
-  content_type?: string;
+  checking_type?: string;
   name: string;
   parser_name?: string;
   required: boolean;
